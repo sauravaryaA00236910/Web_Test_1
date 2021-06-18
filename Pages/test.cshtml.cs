279a04ac58d27169db0e2622d45dcfef10cfe17e
sauravarya <A00236910@mycambrian.ca>
2021-06-18 07:40:22 +0530

    initial commit

---
+++ Pages/test.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_1001_Test.Pages
+{
+    public class testModel : PageModel
+    {
+        public void OnGet()
+        {
+            if(firstNumber>0 && secondNumber > 0)
+            {
+                Result = firstNumber*secondNumber;
+                ResultSet = true;
+            }
+        }
+        [FromQuery(Name = "firstNumber")]
+        public int firstNumber { get; set; }
+
+        [FromQuery(Name = "secondNumber")]
+        public int secondNumber { get; set; }
+
+        public int Result { get; set; }
+
+        public bool ResultSet { get; set; }
+    }
+}
